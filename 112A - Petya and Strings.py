3ef36c0a4b72504a4f90dedbfc69830dea6186f3
Mousa mahmood <102973873+Mousa-hilal@users.noreply.github.com>
2023-03-08 16:58:18 +0300

    112A - Petya and Strings

---
+++ 112A - Petya and Strings.py
@@ -0,0 +1,10 @@
+# user: Mousa-mahmood-hilal
+#Problem: codeforces 112A - Petya and Strings
+s=input().lower()
+s1=input().lower()
+if s>s1:
+    print(1)
+elif s<s1:
+    print(-1)
+else:
+    print(0)
